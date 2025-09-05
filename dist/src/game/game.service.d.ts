da229eb8f40e99800c2e216589026d451d9739b3
Tristan <turgytristan@gmail.com>
2025-09-05 10:26:02 +0200

    Supp code qui ne doit pas être push

--- dist/src/game/game.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Game } from './interfaces/game.interface';
-import { Socket } from 'socket.io';
-export declare class GameService {
-    private game;
-    private clients;
-    init(game: Game): void;
-    addClient(client: Socket): void;
-    removeClient(client: Socket): void;
-}
