5ed12b9e0575b48098b15f2ebf2425c7dd51b373
Tristan <turgytristan@gmail.com>
2025-09-18 14:54:29 +0200

    Suppression des commentaires JSON d'exemple dans AvatarDto pour alléger le code.

--- src/events/players/dto/avatar.dto.ts
+++ src/events/players/dto/avatar.dto.ts
@@ -1,20 +1,3 @@
-//  LOG  Avatar JSON: {
-//   "sex": "woman",
-//   "faceColor": "#F9C9B6",
-//   "earSize": "big",
-//   "eyeStyle": "smile",
-//   "noseStyle": "short",
-//   "mouthStyle": "peace",
-//   "shirtStyle": "polo",
-//   "glassesStyle": "none",
-//   "hairColor": "#506AF4",
-//   "hairStyle": "womanLong",
-//   "hatStyle": "none",
-//   "hatColor": "#506AF4",
-//   "eyeBrowStyle": "upWoman",
-//   "shirtColor": "#9287FF",
-//   "bgColor": "#F4D150"
-// }
 export class AvatarDto {
   sex?: string;
   faceColor?: string;
